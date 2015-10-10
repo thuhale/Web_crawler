c4b5bfb92bbce5c5bb7d937a1a0d935df991a9a4
thuhale <thuhale@stat.wisc.edu>
2015-10-10 14:22:46 -0500

    add AmazonString

--- subset.py
+++ subset.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import codecs
+
 df = pd.read_csv('unicodeProduct.csv', encoding = 'utf-8')
 
 small_df = df[['Brand', 'Title', 'Colors']]
 
 small_df.to_csv('ToSearchOnAmazon.csv', index = False)
+
+fi = codecs.open('ToSearchOnAmazon.txt', 'rU', 'utf-8')
+fo = open('AmazonString.txt','w')
+
+
+for line in fi:
+	string = str(line)
+	new_string = string.replace(',',' ')
+	newer_string = new_string.replace(' ','+')
+	print newer_string
+	fo.write(newer_string + "\n")
